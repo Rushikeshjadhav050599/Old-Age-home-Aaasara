b813505f812a9c98608f8bbde944b7e2f92418bd
Rushikeshjadhav050599 <rjrushijadhav050599u@gmail.com>
2023-01-07 00:33:46 +0530

    first commit

---
+++ assets/js/config.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAom1k94H8YDuV3nGxidxfDLMrFWlf_Id0",
+  authDomain: "aasara-oah.firebaseapp.com",
+  projectId: "aasara-oah",
+  storageBucket: "aasara-oah.appspot.com",
+  messagingSenderId: "646442223461",
+  appId: "1:646442223461:web:8b169dbad924c113a7838c",
+  measurementId: "G-7PZB2VYTQX"
+};
+  
+  firebase.initializeApp(firebaseConfig);
